ace2427db87b6200f3eaccc260360500f990a361
John Brayton <jbrayton@goldenhillsoftware.com>
2023-05-29 20:32:54 -0400

    Added comments around `JBRBundleSchemeHandler`.

--- Read/Reader Window/JBRBundleSchemeHandler.h
+++ Read/Reader Window/JBRBundleSchemeHandler.h
@@ -10,6 +10,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// HTML loaded from a string into a WKWebView cannot directly access CSS files or other resources
+// of an app bundle or other local file. Therefore the CSS files are loaded from
+// "bundle://" URLs This scheme handler is responsible for loading content for those URLs directly from
+// the app bundle.
 @interface JBRBundleSchemeHandler : NSObject<WKURLSchemeHandler>
 
 @end
